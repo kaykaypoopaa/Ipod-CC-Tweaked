6bc50cc02ec297a41eeee03c8a88258ddcc84400
GlitchGod42 <tobikmotyka54@gmail.com>
2025-12-27 15:38:50 +0100

    Create ipodmenu.lua

---
+++ ipodsys/ipodmenu.lua
@@ -0,0 +1,113 @@
+print("welcome to the ipod 1.0!")
+local option = 1
+local selecting = true
+local file = fs.open("settings.txt", "r")
+local Username = file.readLine()
+local ID = file.readLine()
+local theme = file.readLine()
+if theme == "Dark" then
+    term.setBackgroundColor("f")
+elseif theme == "Medium" then
+    term.setBackgroundColor(8)
+elseif theme == "Light" then
+    term.setBackgroundColor(1)
+    term.setTextColor("f")
+end
+local modem = peripheral.find("speaker")
+    if moden ==
+local function ListenForKS()
+     repeat
+        local event, key, is_held = os.pullEvent(key)
+        if key == 264 and selecting then
+            option = option + 1
+            if option > 4 then
+                option = 1
+            end
+            sleep(0.5)
+        elseif key == 265 and selecting then
+            option = option - 1
+            if option < 1 then
+                option = 4
+            end
+            sleep(0.5)
+        end
+    until selecting == false
+end
+
+local function RenderSel()
+    repeat
+        if option == 1 and selecting then -- highlight option 1
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to ipod os 1.0")
+            print("Selection:")
+            term.blit("Music", "00000", "77777")
+            print()
+            print("Games")
+            print("Text")
+            print("Settings")
+        elseif option == 2 and selecting then -- highlight option 2
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to ipod os 1.0")
+            print("Selection:")
+            print("Music")
+            term.blit("Games", "00000", "77777")
+            print()
+            print("Text")
+            print("Settings")
+        elseif option == 3 and selecting then
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to ipod os 1.0")
+            print("Selection:")
+            print("Music")
+            print("Games")
+            term.blit("Text", "0000", "7777")
+            print()
+            print("Settings")
+        elseif option == 4 and selecting then
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to ipod os 1.0")
+            print("Selection:")
+            print("Music")
+            print("Games")
+            print("Text")
+            term.blit("Settings", "00000000", "77777777")
+        end
+        sleep(0.2)
+    until selecting == false
+end
+
+local function EnterApplication()
+    while true do
+        sleep(0.1)
+        local event, key, is_held = os.pullEvent(key)
+        if key == 257 then
+            sleep(0.5)
+            selecting = false
+            term.clear()
+            term.setCursorPos(1,1)
+            term.write("Opening ")
+            sleep(0.5)
+            if option == 1 then
+                print("Music Program")
+                shell.run("music.lua")
+            elseif option == 2 then
+                print("Games Menu")
+                shell.run("games.lua")
+            elseif option == 3 then
+                print("Messenger App")
+                shell.run("messenger.lua")
+            elseif option == 4 then
+                print("Settings App")
+                shell.run("settings.lua")
+            end
+            os.reboot()
+        end
+    end
+end
+
+sleep(0.1)
+parallel.waitForAll(ListenForKS, RenderSel, EnterApplication)
