cb896d74f4a1c06d1e21c2b3763ff4a994a34585
kaykaypoopaa <kaykaypoopaa@outlook.com>
2025-12-28 04:58:44 -0600

    Create appmenu.lua

---
+++ ipodsys/appmenu.lua
@@ -0,0 +1,115 @@
+print("this is the app menu")
+local option = 1
+local selecting = true
+
+local sets = fs.open("settings.txt", "r")
+sets.readLine()
+sets.readLine()
+local theme = sets.readLine()
+if theme == "Dark" then
+    term.setBackgroundColor(colors.black)
+    term.setTextColor(colors.white)
+elseif theme == "Medium" then
+    term.setBackgroundColor(colors.lightGray)
+    term.setTextColor(colors.gray)
+elseif theme == "Light" then
+    term.setBackgroundColor(colors.white)
+    term.setTextColor(colors.black)
+end
+
+local function ListenForKS()
+    repeat
+        local event, key, is_held = os.pullEvent(key)
+        if key == 264 and selecting then
+            option = option + 1
+            if option > 4 then
+                option = 1
+            end
+            sleep(0.5)
+        elseif key == 265 and selecting then
+            option = option - 1
+            if option < 1 then
+                option = 4
+            end
+            sleep(0.5)
+        end
+    until selecting == false
+end
+
+local function RenderSel()
+    repeat
+        if option == 1 and selecting then -- highlight option 1
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to the app selector!")
+            print("Selection:")
+            term.blit("Calculator", "0000000000", "7777777777")
+            print()
+            print("Notes")
+            print("App Store")
+            print("Back")
+        elseif option == 2 and selecting then -- highlight option 2
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to the app selector!")
+            print("Selection:")
+            print("Calculator")
+            term.blit("Notes", "00000", "77777")
+            print()
+            print("App Store")
+            print("Back")
+        elseif option == 3 and selecting then
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to the app selector!")
+            print("Selection:")
+            print("Calculator")
+            print("Notes")
+            term.blit("App Store", "000000000", "777777777")
+            print()
+            print("Back")
+        elseif option == 4 and selecting then
+            term.clear()
+            term.setCursorPos(1,1)
+            print("Welcome to the app selector!")
+            print("Selection:")
+            print("Calculator")
+            print("Notes")
+            print("App Store")
+            term.blit("Back", "0000", "7777")
+        end
+        sleep(0.2)
+    until selecting == false
+end
+
+local function EnterApplication()
+    while true do
+        sleep(0.1)
+        local event, key, is_held = os.pullEvent(key)
+        if key == 257 then
+            sleep(0.5)
+            selecting = false
+            term.clear()
+            term.setCursorPos(1,1)
+            term.write("Opening ")
+            sleep(0.5)
+            if option == 1 then
+                print("Calculator")
+                shell.run("calc.lua")
+            elseif option == 2 then
+                print("Notes")
+                shell.run("notes.lua")
+            elseif option == 3 then
+                print("App Store")
+                shell.run("appstore.lua")
+            elseif option == 4 then
+                print("Menu")
+                shell.run("ipodmenu.lua")
+            end
+            os.reboot()
+        end
+    end
+end
+
+sleep(0.1)
+parallel.waitForAll(ListenForKS, RenderSel, EnterApplication)
