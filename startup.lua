910b24858d9528c10abe8b2759ad2c525408c8b0
GlitchGod42 <tobikmotyka54@gmail.com>
2026-01-02 14:58:03 +0100

    added update check and changed version.txt to 1.1 for testing purposes

--- startup.lua
+++ startup.lua
@@ -1,4 +1,18 @@
 -- THIS CODE IS NOT LABELED FOR INDIVIDUAL USE.
+local function UpdateCheck()
+  local localversion = "1.0"
+  local file = shell.run("wget https://raw.githubusercontent.com/kaykaypoopaa/Ipod-CC-Tweaked/refs/heads/main/version.txt version.txt")
+    file.readLine()
+if file >= localversion then
+  print("Do you want to update?")
+  local updatereq = read()
+elseif updatereq == y then
+  shell.run(updater.lua)
+elseif updatereq == n then
+    end
+  end
+end
+
 local function RunIpod()
   shell.run("cd ipodsys")
   shell.run("ipodmenu.lua")
@@ -13,4 +27,4 @@ local function ListenForTerm()
   end
 end
 
-parallel.waitForAny(RunIpod, ListenForTerm)
+parallel.waitForAny(RunIpod, ListenForTerm, UpdateCheck)
