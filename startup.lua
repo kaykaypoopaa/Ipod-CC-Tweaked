a28eadc4eb75ed132f13e459179b866dd350686b
kaykaypoopaa <kaykaypoopaa@outlook.com>
2026-01-03 01:34:38 -0600

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,18 +1,4 @@
 -- THIS CODE IS NOT LABELED FOR INDIVIDUAL USE.
-local function UpdateCheck()
-  local localversion = "1.0"
-  local file = shell.run("wget https://raw.githubusercontent.com/kaykaypoopaa/Ipod-CC-Tweaked/refs/heads/main/version.txt version.txt")
-    file.readLine()
-if file >= localversion then
-  print("Do you want to update?")
-  local updatereq = read()
-elseif updatereq == y then
-  shell.run(updater.lua)
-elseif updatereq == n then
-    end
-  end
-end
-
 local function RunIpod()
   shell.run("cd ipodsys")
   shell.run("ipodmenu.lua")
@@ -27,4 +13,14 @@ local function ListenForTerm()
   end
 end
 
-parallel.waitForAny(RunIpod, ListenForTerm, UpdateCheck)
+local localversion = "1.0"
+local file = shell.run("wget https://raw.githubusercontent.com/kaykaypoopaa/Ipod-CC-Tweaked/refs/heads/main/version.txt version.txt")
+file.readLine()
+if file >= localversion then
+  print("Do you want to update? (y/n)")
+  local updatereq = read()
+elseif updatereq == y then
+  shell.run(updater.lua)
+elseif updatereq == n then
+end
+parallel.waitForAny(RunIpod, ListenForTerm)
