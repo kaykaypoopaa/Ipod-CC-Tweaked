809c8e6e2de9a71d5b253aeefc82f1e9055d45d7
GlitchGod42 <tobikmotyka54@gmail.com>
2026-01-31 17:22:01 +0100

    added app store to installer and fixed updater

--- ipodsys/appstore2.lua
+++
@@ -1,5 +0,0 @@
---0. Variables
-local appstore = "https://github.com/kaykaypoopaa/Ipod-CC-Tweaked/tree/0c595dd04fa0747167b0fe23cd7bf5cde13603d2/appstore"
---1. Fetching stuff
-shell.run("wget https://raw.githubusercontent.com/kaykaypoopaa/Ipod-CC-Tweaked/refs/heads/main/appstore/applist.txt ipodapps/applist.txt")
-
